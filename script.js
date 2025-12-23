72e227a67bda467fbbd39dc7cea5d18be602bb52
anideva <anidevarchowdhury@gmial.com>
2025-12-23 14:30:19 +0530

    added all the js components that is required as per the question

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
 function createTable() {
     //Write your code here
-  
+  //ask user for no of rows
+  let rn = prompt ("Input number of rows");
+  //ask user for the no of columns
+  let cn = prompt ("Input number of columns");
+
+  //convert input to numbers
+  rn = Number(rn);
+  cn = Number(cn);
+
+  //check for the non numeric input 
+if (isNaN(rn) || isNaN(cn)) {
+    return; //this will ignore the input and do nothing 
+}
+//check for zero or negative values
+if (rn<=0 ||cn<=0) {
+    alert("Rows and Columns must be greater than 0")
+    return;
+}
+//get the table element 
+const table= document .getElementById("myTable");
+//table content
+table.innerHTML="";
+//create rows
+for (let i=0;i<rn;i++){
+    const row = table.insertRow();
+    //create columns
+    for (let j=0;j<cn; j++){
+        const cell= row.insertcell();
+        cell.innerText=`Row-${i} Column-$(j)`;
+    }
+}
 }
